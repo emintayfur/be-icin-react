2cf1ec51849c3c019ceba82a89fe0b2fa647e574
Emin <emin.tayfur@path.com.tr>
2023-03-29 20:12:49 +0300

    Kullanılmayan dosyalar silindi, uygulama için stiller kodlandı.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,57 @@
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Merhaba dünya!
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <main className="container">
+            <div className="todo-box">
+                <h3>YapılacaklarListesi</h3>
+
+                <div className="add-todo-input-container">
+                    <input
+                        className="text-input"
+                        placeholder="Bugün neler yapıyoruz :)"
+                    />
+
+                    <button className="add-btn">
+                        Ekle
+                    </button>
+                </div>
+
+                <ul className="todo-s">
+                   <li className="todo">
+                       <div>
+                           <span>Perşembe günü 12.00'deki eğitime katıl.</span>
+                       </div>
+
+                       <div className="todo-actions">
+                           <button className="todo-btn done">
+                               <span>Tamam</span>
+                           </button>
+                           <button className="todo-btn remove">
+                               <span>Sil</span>
+                           </button>
+                       </div>
+                   </li>
+
+                    <li className="todo">
+                        <div className="ok">
+                            <span>Eğitim için makale yaz.</span>
+                        </div>
+
+                        <div className="todo-actions">
+                            <button className="todo-btn done">
+                                <span>Tamam</span>
+                            </button>
+                            <button className="todo-btn remove">
+                                <span>Sil</span>
+                            </button>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </main>
+    );
 }
 
 export default App;
